474ea189f22be6276c2b0c6cfa14c5b2fa276182
antcho92 <antcho92@gmail.com>
2016-09-24 08:00:49 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+var Alexa = require('alexa-sdk');
+
+var APP_ID = "amzn1.ask.skill.8d1845f1-7aa2-4379-aa86-2337a6edc77d"; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var SKILL_NAME = 'Quote Machine';
+
+
+// Quotes module
+var quotes = require('./quotes');
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetFact');
+    },
+    'GetNewFactQuote': function () {
+        this.emit('GetQuote');
+    },
+    'GetQuote': function () {
+        // Get a random quote from the quotes list
+        var quotesIndex = Math.floor(Math.random() * quotes["quotes"].length);
+        var randomQuote = quotes["quotes"][quotesIndex];
+
+        // Create speech output
+        var speechOutput = "Here's a quote by: "+ randomQuote['author'] + ":, " + randomQuote['quote'];
+
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME)
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = "You can say tell me a quote, or, you can say exit... What can I help you with?";
+        var reprompt = "What can I help you with?";
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    }
+};
---
+++ quotes.js
@@ -0,0 +1,55 @@
+module.exports = {"quotes": [
+    {
+        "author": "Henry David Thoreau",
+        "quote": "If you would convince a man that he does wrong, do right. Men will believe what they see."
+    },
+    {
+        "author": "Shana Alexander",
+        "quote": "The sad truth is that excellence makes people nervous."
+    },
+    {
+        "author": "Walt Whitman",
+        "quote": "If any thing is sacred the human body is sacred."
+    },
+    {
+        "author": "Dr. Seuss",
+        "quote": "Don't cry because it's over, smile because it happened."
+        },
+    {
+        "author": "Edmund Hillary",
+        "quote": "It is not the mountain we conquer, but ourselves."
+    },
+    {
+        "author": "Martin Luther King, Jr.",
+        "quote": "The function of education is to teach one to think intensively and to think critically. Intelligence plus character - that is the goal of true education."
+    },
+    {
+        "author": "Albert Einstein",
+        "quote": "Look deep into nature, and then you will understand everything better."
+    },
+    {
+        "author": "Oscar Wilde",
+        "quote": "Be yourself; everyone else is already taken."
+    },
+    {
+        "author": "Mahatma Gandhi",
+        "quote": "Be the change that you wish to see in the world."
+    },
+    {
+        "author": "Maya Angelou",
+        "quote": "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel."
+    },
+    {
+        "author": "Mark Twain",
+        "quote": "I don't like to commit myself about heaven and hell-you see, I have friends in both places."
+    }
+]};
+
+
+/*   template
+    {
+        "author": "",
+        "quote": ""
+    },
+
+*/
